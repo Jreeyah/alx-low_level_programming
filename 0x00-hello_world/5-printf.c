78a7e00d9f8dcb518f7a69afcac8f91a2965b7ea
jreeyah <judithnaza03@gmail.com>
2023-08-10 12:39:20 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar , but the out come is a piece of art,\n");
+	return (0);
+}
